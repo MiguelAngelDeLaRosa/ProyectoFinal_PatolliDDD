61e6f6348d4f28723dc4478afd275f57ac6a8fae
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-12-06 08:15:46 -0700

    Aplicacion
    Se creó el proyecto patolliAplicacion, el cuál utilizará los casos de uso identifiicados para comunicar a la UI con la capa de servicios

---
+++ patolliAplicacion/src/servicios/Servicios.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package servicios;
+
+/**
+ *
+ * @author edw_v
+ */
+public class Servicios {
+
+    public void crearPartida(){
+        
+    }
+    
+    public void unirseAPartida(){
+        
+    }
+    
+    public void iniciarPartida(){
+        
+    }
+    
+    public void salirDePartida(){
+        
+    }
+    
+    public void ejercerTurno(int valorObtenidoAlLanzarCañas){
+        
+    }
+}
