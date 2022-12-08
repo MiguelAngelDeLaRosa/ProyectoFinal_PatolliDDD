4938afef8f741559146d822ff30952ecdc4aca02
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-12-08 13:33:06 -0700

    Comentarios patolliDominioServicios
    Se agregaron comentarios a las clases del proyecto patolliDominioServicios

--- patolliDominioServicios/src/servicios/Servicios.java
+++ patolliDominioServicios/src/servicios/Servicios.java
@@ -21,20 +21,57 @@ public class Servicios {
     private int cantidadDeJugadores;
     private int cantidadDePuntosPorJugador;
 
+    /**
+     * Constructor que inicializa el atributo idServicio al valor del parámetro
+     * recibido.
+     *
+     * @param idServicio entero con el id del servicio.
+     */
     public Servicios(int idServicio) {
         this.idServicio = idServicio;
     }
 
+    /**
+     * Inicializa algunos atributos de la clase al valor de sus parámetros y
+     * crea una instancia de partida para asignarla al atributo de la clase.
+     *
+     * @param numeroDeCasillasPorAspa entero con el número de casillas por aspa.
+     * @param cantidadDeJugadores entero que indica la cantidad de jugadores que
+     * tendrá la partida.
+     * @param cantidadDePuntosPorJugador entero que indica la cantidad de puntos
+     * que tendrá cada jugador.
+     * @param montoPorApuesta entero que indica el monto que se cobrará por
+     * apuesta.
+     * @param nombreDelJugador String que indica el nombre del jugador.
+     * @param colorDeFichaDelJugador Color que indica el color de la ficha del
+     * jugador.
+     */
     public void crearPartida(int numeroDeCasillasPorAspa, int cantidadDeJugadores, int cantidadDePuntosPorJugador, int montoPorApuesta, String nombreDelJugador, Color colorDeFichaDelJugador) {
         partida = new AggregatePartida(idDePartida, numeroDeCasillasPorAspa, false, cantidadDePuntosPorJugador, montoPorApuesta, nombreDelJugador, colorDeFichaDelJugador);
         this.cantidadDeJugadores = cantidadDeJugadores;
         this.cantidadDePuntosPorJugador = cantidadDePuntosPorJugador;
     }
 
+    /**
+     * Llama al método para agregar a un jugador en el AggregatePartida.
+     *
+     * @param nombreDelJugador String que indica el nombre del jugador.
+     * @param colorDeFichaDelJugador Color que indica el color de la ficha del
+     * jugador.
+     */
     public void unirseAPartida(String nombreDelJugador, Color colorDeFichaDelJugador) {
         partida.agregarJugador(nombreDelJugador, colorDeFichaDelJugador, cantidadDePuntosPorJugador);
     }
 
+    /**
+     * Llama al método sePuedeIniciarPartida() para corroborar que se pueda
+     * iniciar la partida. En caso de poderse, establece el atributo
+     * laPartidaHaIniciado del tablero de la partida y llama al método para
+     * asignar las casillas de entrada y salida de la lista de jugadores
+     * guardada en la partida.
+     *
+     * @return true en caso de iniciarse la partida, false en caso contrario.
+     */
     public boolean iniciarPartida() {
         boolean sePuedeIniciarPartida = coincideCantidadDeJugadoresConJugadoresAgregados();
 
@@ -42,38 +79,41 @@ public class Servicios {
             partida.setLaPartidaHaIniciado(true);
             partida.asignarCasillasDeEntradaYSalidaAJugadores();
         }
-        
+
         return sePuedeIniciarPartida;
     }
 
+    /**
+     *
+     */
     public void salirDePartida() {
 
     }
 
+    /**
+     *
+     * @param valorObtenidoAlLanzarCañas
+     * @param casilla
+     */
     public void ejercerTurno(int valorObtenidoAlLanzarCañas, Casilla casilla) {
 
     }
 
+    /**
+     *
+     * @param casilla
+     */
     private void asignarFichaACasilla(Casilla casilla) {
 
     }
 
-    public AggregatePartida getPartida() {
-        return partida;
-    }
-
-    public void setPartida(AggregatePartida partida) {
-        this.partida = partida;
-    }
-
-    public int getCantidadDeJugadores() {
-        return cantidadDeJugadores;
-    }
-
-    public void setCantidadDeJugadores(int cantidadDeJugadores) {
-        this.cantidadDeJugadores = cantidadDeJugadores;
-    }
-
+    /**
+     * Indica si la cantidad de jugadores que se encuentran en la lista coincide
+     * con la cantidad de jugadores que debe haber en la partida para poder
+     * iniciar la partida.
+     *
+     * @return true en caso de coincidir, false en caso contrario.
+     */
     public boolean coincideCantidadDeJugadoresConJugadoresAgregados() {
         boolean respuesta = false;
 
@@ -83,9 +123,49 @@ public class Servicios {
         return respuesta;
     }
 
+    /**
+     * Retorna el tablero de la partida.
+     *
+     * @return tablero de la partida.
+     */
     public Tablero enviarTableroAAplicacion() {
         return partida.getTablero();
     }
-    
-    
+
+    /**
+     * Retorna el AggregatePartida.
+     *
+     * @return AggregatePartida con los datos de la partida.
+     */
+    public AggregatePartida getPartida() {
+        return partida;
+    }
+
+    /**
+     * Establece el AggregatePartida.
+     *
+     * @param partida AggregatePartida a establecer en el atributo de la clase.
+     */
+    public void setPartida(AggregatePartida partida) {
+        this.partida = partida;
+    }
+
+    /**
+     * Retorna la cantida de jugadores que debe haber en la partida.
+     *
+     * @return entero con la cantidad de jugadores que debe haber en la partida.
+     */
+    public int getCantidadDeJugadores() {
+        return cantidadDeJugadores;
+    }
+
+    /**
+     * Establece la cantidad de jugadores que debe haber en la partida.
+     *
+     * @param cantidadDeJugadores entero que indica la cantidad de jugadores que
+     * debe haber en la partida.
+     */
+    public void setCantidadDeJugadores(int cantidadDeJugadores) {
+        this.cantidadDeJugadores = cantidadDeJugadores;
+    }
 }
